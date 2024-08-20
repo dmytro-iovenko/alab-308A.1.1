e465ad96982c3ebe13d1e5bdbd5108cd0da8cac9
Dmytro Iovenko <iovenko@gmail.com>
2024-08-20 12:07:59 -0400

    Part 1: Stack Overflow

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("OK");
+/** Part 1: Stack Overflow */
+
+// Declare a global counter variable.
+let counter = 0;
+
+// Create a simple function that increments the variable, and then calls itself recursively.
+function recurse() {
+  // increments the variable
+  counter++;
+  // calls the function recursively
+  recurse();
+}
+
+// Surround the initial function call in a try/catch block.
+try {
+  recurse();
+} catch (err) {
+  // Within the catch, log the error and the value of the counter variable.
+  console.log(err, counter);
+}
